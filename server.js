081125f2763dd712cc73ee7cca16d77e2014ab1c
Mighty Mouse <maclark540@gmail.com>
2018-07-30 19:37:01 -0400

    Fix env PORT declaration

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.evn.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", function(req, res){
     res.end("Hello World")
